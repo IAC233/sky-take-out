cb25aab175452fddce86173d22e8fad416b34a82
Ryan <3535035224@qq.com>
2026-09-11 20:59:12 +0800

    启用禁用员工账号业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -103,9 +103,20 @@ public class EmployeeController {
         log.info("员工分页查询：{}", employeePageQueryDTO);
         PageResult pageResult = employeeService.pageQuery(employeePageQueryDTO);
         return Result.success(pageResult);
-                
-
     }
 
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation(value = "员工账号启禁用")
+    public Result startOrStop(@PathVariable Integer status, Long id){
+        log.info("员工账号启禁用：{}，{}", status, id);
+        employeeService.startOrStop(status, id);
+        return Result.success();
+    }
 
 }

5ac3f97dd5a366aa0008fbbf547d63120bbbf77d
Ryan <3535035224@qq.com>
2026-09-15 19:35:46 +0800

    删除菜品业务代码优化

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -95,12 +95,19 @@ public class DishServiceImpl implements DishService {
         if (setmealIds != null && setmealIds.size() > 0) {
             throw new DeletionNotAllowedException(MessageConstant.DISH_BE_RELATED_BY_SETMEAL);
         }
-        //删除菜品数据
-        for (Long id : ids) {
+
+        /**
+         * //删除菜品数据
+         * for (Long id : ids) {
             dishMapper.deleteById(id);
 
             //删除菜品关联的口味数据
             dishFlavorMapper.deleteByDishId(id);
         }
+         */
+        //根据菜品id集合删除菜品数据
+        dishMapper.deleteByIds(ids);
+        //根据菜品id集合删除菜品口味数据
+        dishFlavorMapper.deleteByDishIds(ids);
     }
 }

b4b738fc548cd0f8bf587d7935648f5c7fbe8b8e
Ryan <3535035224@qq.com>
2026-09-16 10:59:43 +0800

    新增套餐业务代码开发

---
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -0,0 +1,13 @@
+package com.sky.service;
+
+import com.sky.dto.SetmealDTO;
+import com.sky.entity.Setmeal;
+
+public interface SetmealService {
+
+    /**
+     * 新增套餐
+     * @param setmealDTO
+     */
+    void saveWithDish(SetmealDTO setmealDTO);
+}
